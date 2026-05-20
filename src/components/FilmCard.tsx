5804f633fc4eb25f3da58b10da472c6cbc42d150
Matěj Krones <matej.krones@datafriends.cz>
2026-05-20 15:51:00 +0200

    feat: Úkol 1 – komponenta FilmCard s typed props

---
+++ src/components/FilmCard.tsx
@@ -0,0 +1,27 @@
+interface FilmCardProps{
+  title : string;
+  year : number;
+  genre : string;
+  rating : number;
+  watched : boolean;
+  onToggleWatched : (title : string) => void;
+}
+
+function FilmCard({title, year, genre, rating, watched, onToggleWatched}: FilmCardProps){
+  const isRatingValid = rating >= 1 && rating <= 10
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>Rok: {year}</p>
+      <p>Žánr: {genre}</p>
+      {isRatingValid ? (<p>⭐️ {rating}</p>) : (<p>❌ Invalid</p>)}
+      {watched ? (<p>✅ Zhlédnuto</p>) : (<p>❌ Nezhlédnuto</p>)}
+      <button onClick={() => onToggleWatched(title)}>
+        Změnit stav zhlédnutí
+      </button>
+    </>
+  )
+
+}
+export default FilmCard;
