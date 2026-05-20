db7540175ce191ccb853652cffe3c78a8305f13c
Matěj Krones <matej.krones@datafriends.cz>
2026-05-20 11:39:46 +0200

    Inital commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  // Volitelne: alias pro importy
+  resolve: {
+    alias: { '@': '/src' },
+  },
+});
