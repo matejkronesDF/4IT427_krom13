cbfa0694546637d12e89104143661a5697840130
Matěj Krones <matej.krones@datafriends.cz>
2026-05-20 15:54:40 +0200

    chore: Added formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import FilmCard from "./components/FilmCard";
+import FilmCard from './components/FilmCard';
 const films = [
-    {id: "1", title: "Inception", year: 2010, genre: "Sci-Fi", rating: 9, watched: true},
-    {id: "2", title: "The Matrix", year: 1999, genre: "Sci-Fi", rating: 8, watched: false},
-    {id: "3", title: "Interstellar", year: 2014, genre: "Sci-Fi", rating: 9, watched: true},
-  ];
+  { id: '1', title: 'Inception', year: 2010, genre: 'Sci-Fi', rating: 9, watched: true },
+  { id: '2', title: 'The Matrix', year: 1999, genre: 'Sci-Fi', rating: 8, watched: false },
+  { id: '3', title: 'Interstellar', year: 2014, genre: 'Sci-Fi', rating: 9, watched: true },
+];
 function App() {
-  
   return (
-  <>
-    <h1>Films Watchlist</h1>
-    {films.map((film) => (
-      <FilmCard
-        key={film.id}
-        title={film.title}
-        year={film.year}
-        genre={film.genre}
-        rating={film.rating}
-        watched={film.watched}
-        onToggleWatched={(title) => console.log("toggle:", title)}
-      />
-    ))}
-  </>
-);
+    <>
+      <h1>Films Watchlist</h1>
+      {films.map((film) => (
+        <FilmCard
+          key={film.id}
+          title={film.title}
+          year={film.year}
+          genre={film.genre}
+          rating={film.rating}
+          watched={film.watched}
+          onToggleWatched={(title) => console.log('toggle:', title)}
+        />
+      ))}
+    </>
+  );
 }
 
 export default App;
--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -1,27 +1,24 @@
-interface FilmCardProps{
-  title : string;
-  year : number;
-  genre : string;
-  rating : number;
-  watched : boolean;
-  onToggleWatched : (title : string) => void;
+interface FilmCardProps {
+  title: string;
+  year: number;
+  genre: string;
+  rating: number;
+  watched: boolean;
+  onToggleWatched: (title: string) => void;
 }
 
-function FilmCard({title, year, genre, rating, watched, onToggleWatched}: FilmCardProps){
-  const isRatingValid = rating >= 1 && rating <= 10
+function FilmCard({ title, year, genre, rating, watched, onToggleWatched }: FilmCardProps) {
+  const isRatingValid = rating >= 1 && rating <= 10;
 
   return (
     <>
       <h2>{title}</h2>
       <p>Rok: {year}</p>
       <p>Žánr: {genre}</p>
-      {isRatingValid ? (<p>⭐️ {rating}</p>) : (<p>❌ Invalid</p>)}
-      {watched ? (<p>✅ Zhlédnuto</p>) : (<p>❌ Nezhlédnuto</p>)}
-      <button onClick={() => onToggleWatched(title)}>
-        Změnit stav zhlédnutí
-      </button>
+      {isRatingValid ? <p>⭐️ {rating}</p> : <p>❌ Invalid</p>}
+      {watched ? <p>✅ Zhlédnuto</p> : <p>❌ Nezhlédnuto</p>}
+      <button onClick={() => onToggleWatched(title)}>Změnit stav zhlédnutí</button>
     </>
-  )
-
+  );
 }
 export default FilmCard;
